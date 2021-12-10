de1e15ba38a2e1f92984d29415b8268c1cd49cd7
Fredrik Oseberg <fredrik.no@gmail.com>
2021-12-10 09:13:25 +0100

    fix: reexport unleash-proxy-client

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
-export type { IConfig } from 'unleash-proxy-client';
+/** @format */
+
+export type {
+  IConfig,
+  IContext,
+  IMutableContext,
+  IVariant,
+  IToggle,
+} from 'unleash-proxy-client';
+export {
+  UnleashClient,
+  IStorageProvider,
+  LocalStorageProvider,
+  InMemoryStorageProvider,
+} from 'unleash-proxy-client';
+
 import FlagProvider from './FlagProvider';
 import useFlag from './useFlag';
 import useFlagsStatus from './useFlagsStatus';
 import useVariant from './useVariant';
 import useUnleashContext from './useUnleashContext';
 
-export { FlagProvider, useFlag, useFlagsStatus, useVariant, useUnleashContext };
+export {
+  FlagProvider,
+  useFlag,
+  useFlagsStatus,
+  useVariant,
+  useUnleashContext,
+};
 
 export default FlagProvider;
