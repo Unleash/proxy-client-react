6452ab65be90e20ba65490076b39e29959d53236
Jeff Fairley <jfairley@gmail.com>
2021-12-09 18:51:31 -0700

    refactor: remove deferred function calls by using 'useMemo'

--- src/FlagProvider.tsx
+++ src/FlagProvider.tsx
@@ -7,58 +7,26 @@ interface IFlagProvider {
 }
 
 const FlagProvider: React.FC<IFlagProvider> = ({ config, children }) => {
-  const [client, setClient] = React.useState(null);
-  const functionCalls = React.useRef<any>();
-  functionCalls.current = [];
-
-  React.useEffect(() => {
+  const client = React.useMemo(() => {
     const client = new UnleashClient(config);
-
-    functionCalls.current.forEach((call: any) => {
-      call(client);
-    }, []);
-
-    functionCalls.current = [];
-
-    setClient(client);
-
     client.start();
+    return client;
   }, []);
 
   const updateContext = async (context: IContext): Promise<void> => {
-    if (!client) {
-      deferCall(async (client: any) => await client.updateContext(context));
-      return;
-    }
     await client.updateContext(context);
   };
 
-  const deferCall = (callback: (client: any) => void) => {
-    functionCalls.current.push(callback);
-  };
-
   const isEnabled = (name: string) => {
-    if (!client) {
-      deferCall((client: any) => client.isEnabled(name));
-      return;
-    }
     return client.isEnabled(name);
   };
 
   const getVariant = (name: string) => {
-    if (!client) {
-      deferCall((client: any) => client.getVariant(name));
-      return {};
-    }
     return client.getVariant(name);
   };
 
-  const on = (event:string, ...args:any[]) => {
-    if (!client) {
-      deferCall((client: any) => client.on(event,...args));
-      return;
-    }
-    return client.on(event, ...args);
+  const on = (...args:Parameters<typeof client.on>) => {
+    return client.on(...args);
   };
 
   const context = { on, updateContext, isEnabled, getVariant, client };
