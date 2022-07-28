4840505e9426e5c88aa46d9f54710701e7e1ae15
Nuno Góis <github@nunogois.com>
2022-07-28 08:49:26 +0100

    feat: export FlagContext

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ export {
   InMemoryStorageProvider,
 } from 'unleash-proxy-client';
 
+import FlagContext from './FlagContext';
 import FlagProvider from './FlagProvider';
 import useFlag from './useFlag';
 import useFlagsStatus from './useFlagsStatus';
@@ -22,6 +23,7 @@ import useUnleashContext from './useUnleashContext';
 import useUnleashClient from './useUnleashClient';
 
 export {
+  FlagContext,
   FlagProvider,
   useFlag,
   useFlagsStatus,
